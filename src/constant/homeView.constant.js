cb5bf14dd195748aad5e70f3d2e4bf2f069ed8e5
LAPTOP-OVAJOR4H\user <kilee880409@gmail.com>
2023-07-31 03:33:31 +0800

    修復圖片路徑

--- src/constant/homeView.constant.js
+++ src/constant/homeView.constant.js
@@ -1,24 +1,24 @@
 export const SloganDetails = [
     {
-        icon:"../images/home-section2-1.png",
+        icon:"images/home-section2-1.png",
         alt:"home-section2-1.png",
         title:"單一價格",
         details:"無論任何度數皆不需追加費用即可擁有適合自己的薄型球面鏡片。"
     },
     {
-        icon:"../images/home-section2-2.png",
+        icon:"images/home-section2-2.png",
         alt:"home-section2-2.png",
         title:"20 分鐘即可取件",
         details:"為了您的寶貴時間著想，以豐富專業知識與技術將結帳到交件的時間縮減至最快 20 分鐘即可完成。"
     },
     {
-        icon:"../images/home-section2-3.png",
+        icon:"images/home-section2-3.png",
         alt:"home-section2-3.png",
         title:"安心售後服務",
         details:"我們提供長達 120 天的保固售後服務，不限會員資格皆享有免費深層保養及專業維修服務。"
     },
     {
-        icon:"../images/home-sectioin2-4.png",
+        icon:"images/home-sectioin2-4.png",
         alt:"home-section2-4.png",
         title:"關於鏡片",
         details:"使用世界知名頂級品牌，抗UV、防汙鍍膜薄型非球面鏡片。"
@@ -27,19 +27,19 @@ export const SloganDetails = [
 
 export const Classical = [
     {
-        image:"../images/home-section3-1.png",
+        image:"images/home-section3-1.png",
         alt:"home-section3-1",
         title:"OPTICAL",
         href:""
     },
     {
-        image:"../images/home-section3-2.png",
+        image:"images/home-section3-2.png",
         alt:"home-section3-2",
         title:"SUNGLASSES",
         href:""
     },
     {
-        image:"../images/home-section3-3.png",
+        image:"images/home-section3-3.png",
         alt:"home-section3-3",
         title:"FUNCTIONAL",
         href:""
