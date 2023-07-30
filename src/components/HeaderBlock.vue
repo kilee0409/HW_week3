0f3d064cb938ef997a0dfadf61b88062ce536456
LAPTOP-OVAJOR4H\user <kilee880409@gmail.com>
2023-07-31 03:29:49 +0800

    修復路徑

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -5,7 +5,7 @@
 <template>
     <header class="bg-primary text-secondary flex justify-around items-center md:flex-col md:items-start">
         <div class="logo">
-            <img class="w-[100%]" src="images/logo-white.png" alt="logo">
+            <img class="w-[100%]" src="/images/logo-white.png" alt="logo">
         </div>
         <nav class="flex gap-[65px] lg:text-[20px] md:w-[100%] md:flex-wrap md:gap-0">
             <a class="nav-item" href="#">系列鏡框</a>
